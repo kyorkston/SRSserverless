206a103fd0cc7dd342f604aecbae231777181020
Kristie Yorkston <kyorkston@gmail.com>
2017-10-18 17:05:15 +0100

    add event.json and separate post lambda

--- handler.js
+++ handler.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const postDataToVouchers = (arg) => {
-  console.log(arg);
+const logging = (arg) => {
+  console.log(`User id ${arg} receiving reward.`);
 }
 
-module.exports.getDataFromAPI = (event, context, callback) => {
+module.exports.get = (event, context, callback) => {
 
   // NB: no input for event data, therefore no event object
   console.log(event);
@@ -17,6 +17,24 @@ module.exports.getDataFromAPI = (event, context, callback) => {
     }),
   };
 
-  callback(postDataToVouchers(), event.user_id);
+  /* use callback for logging and then separate lambda to post?
+  add logging into post func?
+  add logging into get func?
+  */
 
+  callback(logging(), event.user_id);
+};
+
+module.exports.post = (event, context, callback) => {
+  console.log(event.user_id);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Posting userId: ${event.user_id} to voucher service.`,
+      input: event.user_id,
+    }),
+  };
+
+  callback(null, response);
 };
