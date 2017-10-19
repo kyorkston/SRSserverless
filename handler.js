fa5bd0bd85a3229a7251d9276034206424a2564a
Kristie Yorkston <kyorkston@gmail.com>
2017-10-19 12:58:15 +0100

    who knows

--- handler.js
+++ handler.js
@@ -1,24 +1,43 @@
 'use strict'
 
+const http = require('http')
+// grab user id and voucher id from CRM
+// post to voucher service with data
+// another lambda to communicate with the services?
+
+
+// const options = {
+//   host: 'localhost',
+//   path: '/path'
+// }
+
+// var res = { "user_id": "12", "voucher_id": "177" }
+
+// http.get(options, (response) => {
+//   res = response.getBody()
+// })
+
 module.exports.get = (event, context, callback) => {
 
   // NB: no input for event data, therefore no event object
-  console.log(event)
 
   const response = {
     statusCode: 200,
+    VAR_1: process.env.VAR_1,
     body: JSON.stringify({
       message: `Grabbing userId: ${event.user_id} from API.`,
       input: event,
     }),
   }
-
   /* use callback for logging and then separate lambda to post?
   add logging into post func?
   add logging into get func?
   */
+  callback = (userId ,voucherId) => {
+    console.log(`User id ${userId} receiving voucher id ${voucherId}.`)
+  }
 
-  callback(logging(), event.user_id);
+  callback(event.user_id, event.voucher_id)
 }
 
 module.exports.post = (event, context, callback) => {
@@ -33,9 +52,4 @@ module.exports.post = (event, context, callback) => {
   }
 
   callback(null, response)
-}
-
-
-const logging = (userId) => {
-  console.log(`User id ${userId} receiving reward.`)
 }
